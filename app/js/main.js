5c46305d3eae1d00eb8f63d1c465b4082fab089b
rockn1989 <gold_100@bk.ru>
2018-04-04 18:45:38 +0300

    Поиск/Выпадающее меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,34 @@
 'use strict';
 
+var CP = CP || {};
+
 $(function() {
 
+	/* SHOW FORM */
+
+	$('.js__search-form').on('click', function(e) {
+		e.preventDefault();
+		$('.header__top-contacts-links').fadeOut('350', function() {
+			$('.search-form').fadeIn('350', function() {
+				$('.search-form').addClass('show').find('input').focus();
+			});
+			$(document).on('click keyup', hiddenSearchForm);
+		});
+	});
+
+	function hiddenSearchForm (e) {
+		var form = document.querySelector('.search-form'),
+			input = document.querySelector('.search-form input'),
+				btn = document.querySelector('js__search-form');
+		if(((e.which == 27) && (e.target != input || e.target != btn)) && $(form).hasClass('show')) {
+			$(form).fadeOut('350', function() {
+				$(input).val('');
+				$(this).removeClass('show');
+				$('.header__top-contacts-links').fadeIn('350');
+				$(document).unbind('click keyup', hiddenSearchForm);
+			});
+		};
+	};
 
 	/* DISABLED UIKIT ANIMATION FOR MOBILE */
 
