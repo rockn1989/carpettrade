315b021e6d2f7d58ad58d3f3f576bc3fe52d178f
rockn1989 <gold_100@bk.ru>
2018-04-06 17:48:59 +0300

    главная страница 2

--- app/js/main.js
+++ app/js/main.js
@@ -6,54 +6,93 @@ $(function() {
 
 	/* MAIN SLIDER */
 
-$('div.preload').each(function(i, el) {
-	var imgSrc = $(el).find('.slide__inner').css('background-image');
-	if(imgSrc) {
-		var src = imgSrc.replace(/(^url\()|(\)$|[\"\'])/g, ''),
-				img = $('<img>').attr('src', src).on('load', function() {
-					$(el).removeClass('preload');
-				});
-	}
-});
-
-$('.main-slider').slick({
-	arrows: true,
-	dots: true,
-	infinity: true,
-	cssEase: 'linear',
-	autoplay: true,
-	fade: true,
-	autoplaySpeed: 2000,
-	speed: 500,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	prevArrow: '<div class="btn-slide slick-prev"><i class="icon-arrow-left"></i></div>',
-	nextArrow: '<div class="btn-slide slick-next"><i class="icon-arrow-right"></i></div>',
-	responsive: [
-		{
-			breakpoint: 1025,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: true,
-			}
-		},
-		{
-			breakpoint: 940,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+	$('div.preload').each(function(i, el) {
+		var imgSrc = $(el).find('.slide__inner').css('background-image');
+		if(imgSrc) {
+			var src = imgSrc.replace(/(^url\()|(\)$|[\"\'])/g, ''),
+					img = $('<img>').attr('src', src).on('load', function() {
+						$(el).removeClass('preload');
+					});
 		}
-	]
-});
+	});
+
+	$('.main-slider').slick({
+		arrows: true,
+		dots: true,
+		infinity: true,
+		cssEase: 'linear',
+		autoplay: true,
+		fade: true,
+		autoplaySpeed: 2000,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="btn-slide slick-prev"><i class="icon-arrow-left"></i></div>',
+		nextArrow: '<div class="btn-slide slick-next"><i class="icon-arrow-right"></i></div>',
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 940,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+	/* PRODUCT SLIDER */
+
+	$('.products-slider .sliders-list').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 3,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 1500,
+		centerMode: true,
+		centerPadding: '130px',
+		prevArrow: '<div class="btn-slide slick-prev"><i class="icon-arrow-left"></i></div>',
+		nextArrow: '<div class="btn-slide slick-next"><i class="icon-arrow-right"></i></div>',
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 940,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	})
 
 	/* SHOW FORM */
 
